0b5bf2ed18ee59f79481dcb4752347844adbb3ce
DaVinciTachyon <davincitachyon@gmail.com>
2019-04-05 13:43:28 +0100

    working again?

--- Backend/SwiftlyAPI/SwiftlyAPI/settings.py
+++ Backend/SwiftlyAPI/SwiftlyAPI/settings.py
@@ -103,12 +103,12 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
-    """'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.RemoteUserAuthentication'
-    ),"""
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
+    #'DEFAULT_AUTHENTICATION_CLASSES': (
+    #    'rest_framework.authentication.BasicAuthentication',
+    #    'rest_framework.authentication.SessionAuthentication',
+    #    'rest_framework.authentication.RemoteUserAuthentication'
+    #),
+    #'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
