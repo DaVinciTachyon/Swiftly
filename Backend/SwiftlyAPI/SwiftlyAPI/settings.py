17d93889633eee2a68d4021b931d1eed3c908850
DaVinciTachyon <davincitachyon@gmail.com>
2019-04-11 21:06:57 +0100

    wrote my part of the readme

--- Backend/SwiftlyAPI/SwiftlyAPI/settings.py
+++ Backend/SwiftlyAPI/SwiftlyAPI/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i10$tt&^++wi%99iotv%0$1*3mlu0++yuq6)-7fb$m+a93uh&j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
